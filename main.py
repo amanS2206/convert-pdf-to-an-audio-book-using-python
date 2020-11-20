1a97fa927d006b3c9f73ddf958a7a6d3f2c3adc4
amanS2206 <aman.saxena_cs18@gla.ac.in>
2020-11-21 01:45:15 +0530

    create pyttsx3 object to speak the text

--- main.py
+++ main.py
@@ -21,4 +21,4 @@ with pdfplumber.open(file) as pdf:
     for i in range(0, pages):
         page = pdf.pages[i]
         text = page.extract_text()
-        print(text)
+        speaker  = pyttsx3.init()
